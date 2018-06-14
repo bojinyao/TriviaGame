32198c00d7890fbe7dda9e0f8275ed8bfa7fd6d3
Max Yao <bojinyao@berkeley.edu>
2018-06-13 19:30:59 -0700

    Finished Game Logic, starting to work on displays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
-    const TIMEALLOWED = 30;
-    const SRC = "../images/";
+    const TIMEALLOWED = 15;
+    const SRC = "assets/images/";
 
     class Question {
 
@@ -17,12 +17,30 @@ $(document).ready(function() {
             this.imgSrc = imgSrc;
             this.correct = false;
             this.timeLeft = TIMEALLOWED;
+            this.interval = null;
         }
 
         toString() {
-            return `Question: ${this.questionStr} Answer: ${this.answer} Correct: ${this.correct} TimeLeft: ${this.timeLeft}`
+            return `Question: ${this.questionStr} Answer: ${this.answer}, Correct: ${this.correct}, TimeLeft: ${this.timeLeft}, url: ${this.imgSrc}`
         }
 
+        startCountDown() {
+            this.interval = setInterval(() => {this.countDown()}, 1000);
+        }
+
+        countDown() {
+            this.timeLeft--;
+            updateTimer(this.timeLeft);
+            if (this.timeLeft === 0) {
+                this.clearCountDown();
+                clearStage();
+                displayResult(this);
+            }
+        }
+
+        clearCountDown() {
+            clearInterval(this.interval);
+        }
     }
 
 
@@ -32,14 +50,22 @@ $(document).ready(function() {
     var choicesSec = $(".choices-section");
     var totalWins = 0;
     var totalLoses = 0;
+    var totalQues = 0;
     var quesList = [];
     var quesNum = 0;
+    var gameOver = false;
+
+    function updateTimer(time) {
+        timeSec.html(`<h2>Time Remaining: ${time} Seconds</h2>`);
+    }
+
 
     function stageQuestion(question) {
+        clearStage();
         timeSec.append(`<h2>Time Remaining: ${question.timeLeft} Seconds</h2>`);
         quesSec.append(`<h1>${question.questionStr}</h1>`);
         question.choicesList.forEach(choice => {
-            choicesSec.append(`<button type="button" class="btn btn-secondary btn-lg btn-block">${choice}</button>`);
+            choicesSec.append(`<button type="button" class="btn btn-secondary btn-lg btn-block rounded-0 option">${choice}</button>`);
         });
     }
 
@@ -49,11 +75,41 @@ $(document).ready(function() {
         choicesSec.empty();
     }
 
+    function displayResult(question) {
+        timeSec.html(`<h2>Time Remaining: ${question.timeLeft} Seconds</h2>`);
+        if (question.timeLeft === 0) {
+            quesSec.html(`<h1>Out of Time!</h1>`);
+        } else if (question.correct) {
+            quesSec.html(`<h1>Correct!</h1>`);
+        } else {
+            quesSec.html(`<h1>Nope!</h1>`);
+            choicesSec.html(`<h4>The Correct Answer was: ${question.answer}</h4>`);
+        }
+        choicesSec.append(`<img src=\"${question.imgSrc}\">`);
+
+        setTimeout(function() {
+            if (quesNum < totalQues - 1) {
+                    quesNum++;
+                    stageQuestion(quesList[quesNum]);
+                    quesList[quesNum].startCountDown();
+                
+            } else {
+                quesSec.html(`<h1>All done, Here is how you did!</h1>`);
+                choicesSec.empty();
+                choicesSec.append(`<h4>Correct Answers: ${totalWins}</h4>`);
+                choicesSec.append(`<h4>Incorrect Answers: ${totalLoses}</h4>`);
+                choicesSec.append(`<h4>Unanswered: ${totalQues - totalLoses - totalWins}</h4>`);
+                choicesSec.append(`<button type="button" class="btn btn-outline-success btn-lg btn-block init">Start Over?</button>`);
+            }
+        }, 2000);
+    }
+
     function initialize() {
         totalWins = 0;
         totalLoses = 0;
         quesList = [];
         quesNum = 0;
+        gameOver = false;
         clearStage();
 
         quesList.push( 
@@ -80,23 +136,38 @@ $(document).ready(function() {
             `${SRC}nirvana.gif`)
         );
 
+        totalQues = quesList.length;
+
         quesList.forEach(question => {
             console.log(question.toString());
         });
 
         stageQuestion(quesList[quesNum]);
+        quesList[quesNum].startCountDown();
     }
 
-    /** Initializing Game. */
-    initialize();
+    $(document).on("click", ".init", function() {
+        /** Initializing Game. */
+        initialize();
+    });
 
+    $(document).on("click", ".option", function() {
+        let currQues = quesList[quesNum];
+        let usrChoice = $(this).text();
+        console.log(`User chose: ${usrChoice}`);
+        if (usrChoice == currQues.answer) {
+            totalWins++;
+            currQues.correct = true;
+        } else {
+            totalLoses++;
+            currQues.correct = false;
+        }
+        
+        currQues.clearCountDown();
+        clearStage();
+        displayResult(currQues);
+    });
 
-
-
-
-
-
-
-
+    
 
 });
